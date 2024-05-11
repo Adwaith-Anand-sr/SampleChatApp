5fff00b6db1c5df4a6ccb47a1bafb468c7d237bc
Adwaith-Anand-sr <139732933+Adwaith-Anand-sr@users.noreply.github.com>
2024-05-11 18:00:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,8 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
