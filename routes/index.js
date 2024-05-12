52efce6d3662c4e609c8d4465ff7b273a6a38e0e
Adwaith-anand-sr <sreeadwa@gmail.com>
2024-05-12 19:31:56 +0530

    added socket.io

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+   cors:{
+      // origin: "http://localhost:3000"
+      origin: "https://samplechatapp-wzz2.onrender.com:3000"
+   }
+});
+
+io.on("connection", (socket) => {
+   console.log("a user connected");
+   
+   
+   socket.on('disconnect', () => {
+      console.log('A user disconnected');
+   });
+   
+});
+
+httpServer.listen(4000,()=>{
+   console.log("socket.io running on port 4000");
+});
+
+
 router.get("/", (req, res)=>{
-   res.send("app is running...");
+   res.render("index");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,34 +1,15 @@
-// const https = require('https');
-// const fs = require('fs');
 const express = require('express');
-//const socketIo = require('socket.io');
 const path = require('path');
 const app = express();
 
 const index = require("./routes/index");
 
-// const io = socketIo(server);
-
-// Serve static files
 app.use(express.static(path.join(__dirname, '../public')));
 app.use("/", index)
-// Socket.io logic
-// io.on('connection', (socket) => {
-//  console.log('A user connected');
-//    socket.emit("conneted", "heyy");
-//  // Handle chat messages
-//  socket.on('chat message', (msg) => {
-//     io.emit('chat message', msg);
-//  });
-
-//  // Handle disconnection
-//  socket.on('disconnect', () => {
-//     console.log('User disconnected');
-//  });
-// });
+app.set('view engine', 'ejs');
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
